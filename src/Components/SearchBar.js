5f8c0f800808ef2fb73c1143e60fdee87dbd3adc
Tilmant Rémi <towtowrow@gmail.com>
2019-04-02 11:35:37 +0200

    DropDownMenu added

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,1 +1,34 @@
 import React, { Component } from 'react';
+
+
+class SearchBar extends Component {
+  render() {
+    return (
+
+      < div className="container h-100" >
+
+        {/* Dropdown Menu */}
+        <div className="dropdown justify-content-left h-50">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Genre
+          </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="#">Rock</a>
+              <a className="dropdown-item" href="#">Rap</a>
+              <a className="dropdown-item" href="#">etc etc</a>
+            </div>
+          </div>
+
+        {/* SearchBar */}
+        <div className="d-flex justify-content-right h-50">
+          <div className="searchbar">
+            <input className="search_input" type="text" name="" placeholder="Search..." />
+            <a href="#" className="search_icon"> <i className="fas fa-search"> </i> </a>
+          </div>
+
+        </div>
+      </div >
+    );
+  }
+}
+export default SearchBar;
