7cf294e0dfb8ca6aeea8bfa3f82d14a0ed6a34e8
Sebastien Flamant <sebastienflamant31@gmail.com>
2019-04-02 17:45:42 +0200

    sideBar responsive

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,9 +6,9 @@ import Avatar from 'react-avatar';
 class SideBar extends React.Component {
   render() {
     return (
-      <div className='sidebar my-3 mx-4'>
-        <Avatar className="px-2" name="Réminou Tilmant" size="75" round={true} color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])} />
-        <p className="playItLoud px-2 pt-2">Play it Loud</p>
+      <div className='sidebar'>
+        <Avatar className="px-2 mx-1" name="Réminou Tilmant" size="75" round={true} color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])} />
+        <p className="playItLoud px-3 pt-2">Play it Loud</p>
         <Nav vertical>
           <NavItem>
             <NavLink className="linksidebar" href="#">Accueil</NavLink>
