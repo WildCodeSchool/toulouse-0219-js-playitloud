578f2b3c5e824ee67963cca98b691eb8ef06636a
Tilmant Rémi <towtowrow@gmail.com>
2019-04-03 13:46:54 +0200

    Remplacement composant search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from"./Components/SearchBar";
 import Search from "./Components/Search"; 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
        <Search />
         <header className="App-header">
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,70 +1,70 @@
 import React from 'react';
 import {
-  Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
 } from 'reactstrap';
 
-export default class Example extends React.Component {
+export default class Search extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { name: "Homer Simpson" };
+    this.onChange = this.onChange.bind(this);
 
     this.toggle = this.toggle.bind(this);
     this.state = {
       isOpen: false
     };
   }
+
+  onChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
+
   render() {
     return (
       <div>
 
-        <Navbar color="dark" light expand="md" fixed-top>
-          <NavbarBrand href="/">Musiflix</NavbarBrand>
+        <Navbar color="dark" light expand="md" className="fixed-top">
+          <NavbarBrand href="/">{this.state.value}</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              {/* SearchBar */}
-              <NavItem>
-                <div className="recherche">
-                  <div className="searchbar">
-                    <input className="search_input" type="text" name="" placeholder="Search..." />
-                    <a href="#" className="search_icon"> <i className="fas fa-search"> </i> </a>
-                  </div>
+          <Nav className="ml-auto" navbar>
+            {/* SearchBar */}
+            <NavItem>
+              <div className="recherche mx-4">
+                <div className="searchbar">
+                  <input className="search_input " type="text" placeholder="Search..." onChange={this.onChange} />
+                  <a href="#" className="search_icon"> <i className="fas fa-search"> </i> </a>
                 </div>
-              </NavItem>
-              {/* Dropdown Menu */}
-              <NavItem>
-                <div className="dropdown">
-                  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Genre
-                  </button>
-                  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="#">Rap</a>
-                    <a className="dropdown-item" href="#">Rock</a>
-                    <a className="dropdown-item" href="#">Pop</a>
-                    <a className="dropdown-item" href="#">ASMR</a>
-                    <a className="dropdown-item" href="#">Animaux</a>
-                    <a className="dropdown-item" href="#">+18</a>
-                    <a className="dropdown-item" href="#">Bébéluv</a>
-                    <a className="dropdown-item" href="#">ASMR</a>
-                  </div>
+              </div>
+            </NavItem>
+            {/* Dropdown Menu */}
+            <NavItem>
+              <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle mx-4" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Genre
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a className="dropdown-item" href="#">Rap</a>
+                  <a className="dropdown-item" href="#">Rock</a>
+                  <a className="dropdown-item" href="#">Pop</a>
+                  <a className="dropdown-item" href="#">ASMR</a>
+                  <a className="dropdown-item" href="#">Animaux</a>
+                  <a className="dropdown-item" href="#">+18</a>
+                  <a className="dropdown-item" href="#">Bébéluv</a>
+                  <a className="dropdown-item" href="#">ASMR</a>
                 </div>
-              </NavItem>
-            </Nav>
-          </Collapse>
+              </div>
+            </NavItem>
+          </Nav>
         </Navbar>
       </div>
     );
