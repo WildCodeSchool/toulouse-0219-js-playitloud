103fe480b16a77d8343c83331b7fab0b0d5e80eb
Tilmant Rémi <towtowrow@gmail.com>
2019-04-11 16:24:26 +0200

    convertdate ok

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -15,6 +15,13 @@ export default class AlbumDetails extends React.Component {
     const second = ((MS % 60000) / 1000).toFixed(0);
     return `${minute} min ${second} s`;
   }
+  convertdate = (p) => {
+    const us = p.split("-");
+    const year = us[0];
+    const day = us[2];
+    const month = us[1];
+    return `${day} / ${month} / ${year}`;
+  }
   componentDidMount() {
     this.apiCallById();
   }
@@ -45,8 +52,8 @@ export default class AlbumDetails extends React.Component {
         < h3 > {this.state.albumsInfos.name}</h3 >
         {this.state.albumsInfos && <p>{this.state.albumsInfos.artists[0].name}</p>}
         < p > Label : {this.state.albumsInfos.label}</p >
-        <p>Date de sortie : {this.state.albumsInfos.release_date}</p>
-        <p>{this.state.albumsInfos.total_tracks} titres</p>
+        <p>Date de sortie : {this.convertdate("2018-08-31")}</p>
+        <p>{this.state.albumsInfos.total_tracks} titre(s)</p>
         <p>Popularité : {this.state.albumsInfos.popularity}%</p>
         {
           this.state.albumsInfos && this.state.albumsInfos.tracks.items.map((singleTrack, i) =>
