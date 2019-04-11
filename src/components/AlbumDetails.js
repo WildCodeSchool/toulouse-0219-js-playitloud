31597a5b1d326b51fb29bb3351286c31dc2bacfc
Sebastien Flamant <sebastienflamant31@gmail.com>
2019-04-11 15:25:06 +0200

    Détail album OK bébé

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -4,8 +4,9 @@ export default class AlbumDetails extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      id: "",
-      albumsInfos: "",
+      id: '',
+      albumsInfos: '',
+      tracks: []
     }
     this.apiCallById = this.apiCallById.bind(this);
   }
@@ -32,17 +33,23 @@ export default class AlbumDetails extends React.Component {
 
   }
 
-
   render() {
+
     return (
       <div className="main" style={{ color: 'white' }}>
         {this.state.albumsInfos && <img src={this.state.albumsInfos.images[1].url} alt="pictures" />}
-        <h3>{this.state.albumsInfos.name}</h3>
+        < h3 > {this.state.albumsInfos.name}</h3 >
         {this.state.albumsInfos && <p>{this.state.albumsInfos.artists[0].name}</p>}
-        <p>Label : {this.state.albumsInfos.label}</p>
+        < p > Label : {this.state.albumsInfos.label}</p >
         <p>Date de sortie : {this.state.albumsInfos.release_date}</p>
         <p>{this.state.albumsInfos.total_tracks} titres</p>
         <p>Popularité : {this.state.albumsInfos.popularity}%</p>
+        {
+          this.state.albumsInfos && this.state.albumsInfos.tracks.items.map((singleTrack, i) =>
+            <div>
+              <p key={i}>{singleTrack.track_number}. {singleTrack.name} {Number(((singleTrack.duration_ms) / 60000)).toFixed(0)}min{Number((singleTrack.duration_ms) % 60000 / 1000).toFixed(0)}s</p>
+            </div>)
+        }
       </div >
     );
   };
