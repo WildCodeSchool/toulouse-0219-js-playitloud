078e444c7a2fd3c6be87f5ccb0a3b4a69630b357
Tilmant Rémi <towtowrow@gmail.com>
2019-04-12 11:15:50 +0200

    button fav

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -23,20 +23,23 @@ class Cards extends React.Component {
 
   render() {
     return (
-      <NavLink to={`/details-album/${this.props.id}`} >
-        <div>
-          <figure className="album">
-            <img src={this.props.image} alt="pictures" />
-            <figcaption id={this.props.id} onClick={() => { this.props.click(this.props.id); localStorage.setItem('lastLink', `/details-album/${this.props.id}`) }} >
-              <h3>{this.props.name}</h3>
-              <h5>{this.props.artist}</h5>
-            </figcaption>
-          </figure>
-          <br />
-          <br />
-          <button onClick={() => this.manageButton()} > {this.handleButton()} </button>
-        </div>
+
+      <div><NavLink to={`/details-album/${this.props.id}`} >
+
+        <figure className="album">
+          <img src={this.props.image} alt="pictures" />
+          <figcaption id={this.props.id} onClick={() => { this.props.click(this.props.id); localStorage.setItem('lastLink', `/details-album/${this.props.id}`) }} >
+            <h3>{this.props.name}</h3>
+            <h5>{this.props.artist}</h5>
+
+          </figcaption>
+        </figure>
       </NavLink >
+        <br />
+        <br />
+        <button onClick={() => this.manageButton()} > {this.handleButton()} </button>
+      </div>
+
     )
   }
 
