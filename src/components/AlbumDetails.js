80738673d136c0d99473aa9f2e6934ee721dc44a
Tilmant Rémi <towtowrow@gmail.com>
2019-04-17 11:03:52 +0200

    Bouton concert add

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -12,7 +12,9 @@ export default class AlbumDetails extends React.Component {
       tracks: []
     }
     this.apiCallById = this.apiCallById.bind(this);
+
   }
+
   convertTime = (MS) => {
     const minute = (MS / 60000).toFixed(0);
     const second = ((MS % 60000) / 1000).toFixed(0);
@@ -34,6 +36,7 @@ export default class AlbumDetails extends React.Component {
     })
       .then(response => response.json())
       .then(data => {
+
         this.setState({
           albumsInfos: data
 
@@ -42,23 +45,50 @@ export default class AlbumDetails extends React.Component {
 
   }
 
+
+
+  convertName = (idName) => {
+    const name = idName.replace(" ", "+");
+    return name;
+  }
+
   render() {
+
     return (
+
       <div className="main" style={{ color: 'white' }}>
+
         {this.state.albumsInfos && <img src={this.state.albumsInfos.images[1].url} alt={this.state.albumsInfos.name} />}
         < h3 > {this.state.albumsInfos.name}</h3 >
+
+        <br />
+        {this.state.albumsInfos &&
+          <a target="_blank" rel="noreferrer noopener" href={`https://www.ticketmaster.fr/fr/resultat?ipSearch=${this.state.albumsInfos.artists[0].name}`}>
+            <button>
+              Voir les concerts en France
+            </button>
+          </a>}
+
+        <br />
+        <br />
         {this.state.albumsInfos && <p>{this.state.albumsInfos.artists[0].name}</p>}
         < p > Label : {this.state.albumsInfos.label}</p >
+
         {this.state.albumsInfos && <p>Date de sortie : {convertDate(this.state.albumsInfos.release_date)}</p>}
-        <p>{this.state.albumsInfos.total_tracks} titre(s)</p>
+        < p > {this.state.albumsInfos.total_tracks} titre(s)</p >
         <p>Popularité : {this.state.albumsInfos.popularity}%</p>
         {
           this.state.albumsInfos && this.state.albumsInfos.tracks.items.map((singleTrack, i) =>
             <div>
-              <p key={i}>{singleTrack.track_number}. {singleTrack.name} {this.convertTime(singleTrack.duration_ms)}</p>
+              <p key={i}>{singleTrack.track_number}. {singleTrack.name} {this.convertTime(singleTrack.duration_ms)}
+              </p>
             </div>)
         }
+
       </div >
+
     );
+
   };
+
 }
