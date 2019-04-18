b2cc617c4bbca0687fad935b0e6876e3f0cf4b60
Sebastien Flamant <sebastienflamant31@gmail.com>
2019-04-18 09:01:38 +0200

    correction erreur to compile

---
+++ src/components/CardByPlaylist.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+class CardByPlaylist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonValue: false,
+    }
+    this.manageButton = this.manageButton.bind(this);
+    this.getButtonText = this.getButtonText.bind(this);
+  }
+  manageButton = () => {
+    if (this.props.isFavorite) {
+      this.setState({ buttonValue: true })
+      this.props.removeFavorite(this.props.id)
+    } else {
+      this.setState({ buttonValue: false })
+      this.props.favoriteAlbums(this.props.id);
+    }
+  }
+
+  getButtonText = () => this.props.isFavorite ? 'Remove from favorites' : 'Add to favorites';
+
+  render() {
+    return (
+
+      <div><NavLink to={`/playlist/${this.props.id}`} >
+
+        <figure className="album">
+          <img src={this.props.image} alt="pictures" />
+          <figcaption id={this.props.id} >
+            <h3>{this.props.name}</h3>
+            <h5>{this.props.artist}</h5>
+          </figcaption>
+        </figure>
+      </NavLink >
+        <br />
+        <br />
+        <button onClick={() => this.manageButton()} > {this.getButtonText()} </button>
+      </div>
+
+    )
+  }
+
+}
+
+
+export default CardByPlaylist;
