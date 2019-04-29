9badfa276b9d5895525cc3863b52751e731baf4a
Tilmant Rémi <towtowrow@gmail.com>
2019-04-29 11:06:05 +0200

    update profile page css

--- src/components/DisplayProfile.js
+++ src/components/DisplayProfile.js
@@ -4,13 +4,23 @@ import convertDate from '../functions/convertDate';
 const DisplayProfile = ({ profile }) => {
   const prenomNom = profile.display_name.split(' ');
   return (
-    <div className="DisplayProfil" style={{ color: 'red' }}>
-      <h1>Bienvenue {prenomNom[0]}</h1>
-      <p>Email : {profile.email}</p>
-      <p>Pays : {profile.country}</p>
-      <p>Numéro ID : {profile.id}</p>
-      <p>Date de naissance : {convertDate(profile.birthdate)}</p>
-    </div>
+    <div className="DisplayProfil">
+      <h1>PLAY IT LOUD</h1>
+      <br />
+      <br />
+      <h2>Bienvenue {prenomNom[0]}</h2>
+
+      <br />
+      <div className="PProfile">
+
+        <h3>Vos informations de compte</h3>
+        <br />
+        <p>Email : {profile.email}</p>
+        <p>Pays : {profile.country}</p>
+        <p>Numéro ID : {profile.id}</p>
+        <p>Date de naissance : {convertDate(profile.birthdate)}</p>
+      </div>
+    </div >
   );
 };
 
