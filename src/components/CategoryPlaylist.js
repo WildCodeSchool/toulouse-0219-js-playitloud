61dd1eef66933b7a41d082d8b7ac0808e351bf35
TowTowRow <towtowrow@gmail.com>
2019-05-06 08:40:01 +0200

    test

--- src/components/CategoryPlaylist.js
+++ src/components/CategoryPlaylist.js
@@ -8,7 +8,7 @@ export default class CategoryPlaylist extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      id: '',
+      id: '', 
       categoryInfo: "",
       buttonValue: false,
       removeFromFavoritePlaylist: [],
